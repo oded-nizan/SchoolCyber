f1000fca2864104263854345e8ab9939d5ae55e1
oded-nizan <oded.nizan@gmail.com>
2024-11-10 17:23:49 +0200

    updated for 10.11.24 classwork

---
+++ Classwork-10.11.24/client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def main() -> None:
+    my_socket: socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    my_socket.connect(('127.0.0.1', 8820))
+
+    my_socket.send('Hello'.encode())
+    data: str = my_socket.recv(1024).decode()
+    print(f'The server sent the following data: {data}')
+
+    my_socket.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Classwork-10.11.24/server.py
@@ -0,0 +1,25 @@
+import socket
+
+
+def main() -> None:
+    server_socket: socket = socket.socket()
+
+    server_socket.bind(('0.0.0.0', 8820))
+    server_socket.listen()
+    print('The server is up and running')
+
+    (client_socket, client_address) = server_socket.accept()
+    print('Client connected')
+
+    data: str = client_socket.recv(1024).decode()
+    print(f'Client has sent: {data}')
+
+    reply: str = f'Hello {data}'
+    client_socket.send(reply.encode())
+
+    client_socket.close()
+    server_socket.close()
+
+
+if __name__ == '__main__':
+    main()
